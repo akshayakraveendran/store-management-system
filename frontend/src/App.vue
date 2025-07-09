38a9d41d0fde6934dca844848ce54e906d5c2f4a
Akshaya <akshayaraveendran145@gmail.com>
2025-07-09 20:26:18 +0530

    Added Vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="grid grid-cols-2 justify-center items-center m-4 p-4 border border-blue-500 rounded">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+  </div>
+
+  <!-- <RouterView /> -->
+</template>
