720619588314cab1f2b56a3afc5bd6e0fa5468f2
Akshaya <akshayaraveendran145@gmail.com>
2025-07-10 00:19:17 +0530

    Added Modal

--- frontend/src/views/Inventory.vue
+++ frontend/src/views/Inventory.vue
@@ -1,4 +1,10 @@
 <script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+onMounted(() => {
+      initFlowbite()
+    })
 </script>
 
 <template>
@@ -12,7 +18,7 @@
 <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+        <div class="border relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
             <!-- Modal header -->
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
                 <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
@@ -59,6 +65,4 @@
         </div>
     </div>
 </div> 
-
-    
 </template>
